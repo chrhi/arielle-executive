a6863cf39f89b84126552bb10991c23a85cb62f1
abdellah <Abdellah.chehri14@gmail.com>
2023-10-25 16:01:36 +0200

    add

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const error = ({ error, reset }: { error: string; reset: () => void }) => {
   return (
-    <div className="w-full h-[600px] flex justify-end items-center">
+    <div className="w-full h-[600px] flex flex-col justify-center  items-center">
       <h1 className="text-5xl text-center font-bold">something went wrong </h1>
       <div className="flex items-center h-24 gap-x-4">
         <Button variant="ghost" className="my-4" onClick={reset}>
