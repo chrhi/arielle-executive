13fce37a7f7ec3349bf97e92b488c2a30adfd9ce
abdellah <Abdellah.chehri14@gmail.com>
2023-10-27 20:28:19 +0200

    add

--- src/app/louding.tsx
+++ src/app/louding.tsx
@@ -1,9 +1,23 @@
-import { FC } from "react";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import FadeIn from "@/components/animations/FadeIn";
+import { Skeleton } from "@/components/ui/skeleton";
 
-interface loudingAbdullahProps {}
-
-const louding: FC = () => {
-  return <div></div>;
-};
-
-export default louding;
+export default function Louding() {
+  return (
+    <MaxWidthWrapper>
+      <main>
+        <FadeIn>
+          <section className="min-h-[600px]  w-full flex md:flex-row flex-col  justify-center pt-4 ">
+            <div className="md:w-[70%] mb-4 w-full   pt-4 h-fit min-h-[400px] ">
+              <Skeleton className="w-full h-[500px] rounded-lg" />
+            </div>
+            <div className="md:w-[30%] w-full  h-fit min-h-[400px] ">
+              <Skeleton className="w-[90%] h-[200px] rounded-lg" />
+              <Skeleton className="w-[90%] h-[200px] rounded-lg" />
+            </div>
+          </section>
+        </FadeIn>
+      </main>
+    </MaxWidthWrapper>
+  );
+}
